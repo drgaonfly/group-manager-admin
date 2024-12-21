8434f8b2e1bcbd7a6646952e8936895e95c81d39
lwl <1903445178@qq.com>
2024-12-21 18:21:22 +0800

    feat(notification): add sound notification for new customer alerts

--- src/components/socketNotification/NotificationBadge.tsx
+++ src/components/socketNotification/NotificationBadge.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import { useModel } from '@umijs/max';
 import { useSocketNotification } from './useSocketNotification';
+import { notification } from 'antd';
+import { BellOutlined } from '@ant-design/icons';
+
+// 预加载声音
+const soundSrc = '/sounds/newCustomerBeep.mp3';
+
+const playSound = () => {
+  const sound = new Audio(soundSrc);
+  sound.preload = 'auto';
+  sound.play();
+};
+
+const openNotification = (message: string, description: string, playSoundEnabled: boolean) => {
+  if (playSoundEnabled) {
+    playSound();
+  }
+
+  notification.success({
+    message: message,
+    description: description,
+    duration: 3,
+  });
+};
 
 const NotificationBadge: React.FC = () => {
   const { handleCustomerNew } = useModel('notificationModel');
@@ -8,10 +31,19 @@ const NotificationBadge: React.FC = () => {
   useSocketNotification([
     {
       eventName: 'customerNew',
-      onDataReceived: handleCustomerNew,
+      onDataReceived: (data) => {
+        handleCustomerNew(data);
+        openNotification('New Customer', 'A new customer has been added.', true);
+      },
     },
   ]);
-  return <></>;
+
+  return (
+    <BellOutlined
+      onClick={playSound}
+      style={{ fontSize: '12px', padding: '5px 10px', border: 'none' }}
+    />
+  );
 };
 
 export default NotificationBadge;
