885bf389f8e7d74185674456753880c2d954cec2
efihfaifjw <2218344129@qq.com>
2025-03-24 17:25:29 +0800

    fix: delete langues and translates

--- config/routes.ts
+++ config/routes.ts
@@ -89,25 +89,25 @@ export default [
       },
     ],
   },
-  {
-    path: '/multilingual-management',
-    name: 'list.multilingual-management',
-    icon: 'TranslationOutlined',
-    routes: [
-      {
-        path: '/multilingual-management/languages',
-        name: 'list.languages',
-        component: './MultilingualManagement/Langue',
-        icon: 'TranslationOutlined',
-      },
-      {
-        path: '/multilingual-management/translates',
-        name: 'list.translates',
-        component: './MultilingualManagement/Translate',
-        icon: 'TranslationOutlined',
-      },
-    ],
-  },
+  // {
+  //   path: '/multilingual-management',
+  //   name: 'list.multilingual-management',
+  //   icon: 'TranslationOutlined',
+  //   routes: [
+  //     {
+  //       path: '/multilingual-management/languages',
+  //       name: 'list.languages',
+  //       component: './MultilingualManagement/Langue',
+  //       icon: 'TranslationOutlined',
+  //     },
+  //     {
+  //       path: '/multilingual-management/translates',
+  //       name: 'list.translates',
+  //       component: './MultilingualManagement/Translate',
+  //       icon: 'TranslationOutlined',
+  //     },
+  //   ],
+  // },
   {
     path: '/wallet-deal-records',
     name: 'list.walletDealRecords',
