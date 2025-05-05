c5bef18c680670d4ef0bd2a58d453d08dbb45e79
dragonfly <2237676323@qq.com>
2025-05-05 16:08:03 +0800

    feat: remove sth

--- src/pages/UserType/Customer/components/BasicForm.tsx
+++ src/pages/UserType/Customer/components/BasicForm.tsx
@@ -1,11 +1,6 @@
 import { useIntl } from '@umijs/max';
 import React from 'react';
-import {
-  ProForm,
-  ProFormDigit,
-  ProFormSwitch,
-  ProFormTreeSelect,
-} from '@ant-design/pro-components';
+import { ProForm, ProFormTreeSelect } from '@ant-design/pro-components';
 import { Form, Input } from 'antd';
 import EmployeeSelect from '@/components/employeeSelect';
 import useQueryList from '@/hooks/useQueryList';
@@ -44,76 +39,7 @@ const BasicForm: React.FC<Props> = ({ newRecord, onFinish, values }) => {
         },
       }}
     >
-      <ProForm.Group>
-        <ProFormDigit
-          label={intl.formatMessage({ id: 'liquidRate' })}
-          width="md"
-          name="liquidRate"
-          min={0}
-          fieldProps={{
-            precision: 2,
-          }}
-          rules={[{ required: true, message: '请输入流动倍率' }]}
-        />
-        <ProFormDigit
-          label={intl.formatMessage({ id: 'stakeRate' })}
-          name="stakeRate"
-          width="md"
-          min={0}
-          fieldProps={{
-            precision: 2,
-          }}
-          rules={[{ required: true, message: '请输入质押倍率' }]}
-        />
-      </ProForm.Group>
-
-      <ProForm.Group>
-        <ProFormDigit
-          label={intl.formatMessage({ id: 'usdtBalance', defaultMessage: 'USDT余额' })}
-          name="usdtBalance"
-          width="md"
-          min={0}
-        />
-        <ProFormDigit
-          label={intl.formatMessage({ id: 'usdtStaking', defaultMessage: 'USDT质押' })}
-          name="usdtStaking"
-          width="md"
-          min={0}
-        />
-        <ProFormDigit
-          label={intl.formatMessage({ id: 'usdtPlatform', defaultMessage: 'USDT平台' })}
-          name="usdtPlatform"
-          width="md"
-          min={0}
-        />
-        <ProFormDigit
-          label={intl.formatMessage({ id: 'ethPlatform', defaultMessage: 'ETH平台' })}
-          name="ethPlatform"
-          width="md"
-          min={0}
-        />
-      </ProForm.Group>
-
-      <ProForm.Group>
-        <EmployeeSelect />
-
-        <ProFormSwitch
-          width="md"
-          name="isAuthorized"
-          label={intl.formatMessage({ id: 'accountType', defaultMessage: '账户类型' })}
-          checkedChildren={intl.formatMessage({ id: 'demoAccount' })}
-          unCheckedChildren={intl.formatMessage({ id: 'customer' })}
-          initialValue={false}
-        />
-        <ProFormSwitch
-          width="md"
-          name="isVerified"
-          label={intl.formatMessage({ id: 'authStatus', defaultMessage: '授权状态' })}
-          checkedChildren={intl.formatMessage({ id: 'authorized', defaultMessage: '授权' })}
-          unCheckedChildren={intl.formatMessage({ id: 'unauthorized', defaultMessage: '未授权' })}
-          initialValue={false}
-        />
-      </ProForm.Group>
+      <EmployeeSelect />
 
       <ProFormTreeSelect
         name="parent"
