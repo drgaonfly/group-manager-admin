ab6a5f15b7bc60f57cc234dc5b1ff5656269573b
drgaofly <2237676323@qq.com>
2026-07-12 18:47:34 +0800

    feat: remove unused

--- src/pages/Authorization/components/Show.tsx
+++ src/pages/Authorization/components/Show.tsx
@@ -1,9 +1,6 @@
 import { ProDescriptions, ProDescriptionsItemProps } from '@ant-design/pro-components';
-import BotUserTable from './BotUserTable';
-import GroupTable from './GroupTable';
-import { Modal, Tabs } from 'antd';
-import React, { useState } from 'react';
-import { useIntl } from '@umijs/max';
+import { Modal } from 'antd';
+import React from 'react';
 
 interface Props {
   onClose: (e: React.MouseEvent | React.KeyboardEvent) => void;
@@ -14,19 +11,18 @@ interface Props {
 
 const Show: React.FC<Props> = (props) => {
   const { onClose, open, currentRow, columns: cols } = props;
-  const intl = useIntl();
   const filteredColumns = cols.filter((col) => col.dataIndex !== 'option');
 
-  const [pagination, setPagination] = useState<{ current: number; pageSize: number }>({
-    current: 1,
-    pageSize: 20,
-  });
-
-  const tabItems = [
-    {
-      key: 'basic',
-      label: intl.formatMessage({ id: 'basic_info', defaultMessage: '基本信息' }),
-      children: (
+  return (
+    <Modal
+      open={open}
+      onCancel={onClose}
+      footer={null}
+      width="80%"
+      centered
+      className="rounded-lg overflow-hidden"
+    >
+      {currentRow?._id && (
         <ProDescriptions<API.ItemData>
           column={2}
           title={currentRow?.botName}
@@ -50,38 +46,8 @@ const Show: React.FC<Props> = (props) => {
           }}
           size="small"
           className="custom-descriptions"
+          style={{ marginTop: '20px' }}
         />
-      ),
-    },
-    {
-      key: 'groups',
-      label: intl.formatMessage({ id: 'group', defaultMessage: '群组' }),
-      children: <GroupTable currentRow={currentRow} />,
-    },
-    {
-      key: 'users',
-      label: intl.formatMessage({ id: 'user_count', defaultMessage: '用户数量' }),
-      children: (
-        <BotUserTable
-          botUsers={currentRow?.botUsers || currentRow?.botUserConfigs || []}
-          pagination={pagination}
-          setPagination={setPagination}
-        />
-      ),
-    },
-  ];
-
-  return (
-    <Modal
-      open={open}
-      onCancel={onClose}
-      footer={null}
-      width="80%"
-      centered
-      className="rounded-lg overflow-hidden"
-    >
-      {currentRow?._id && (
-        <Tabs defaultActiveKey="basic" items={tabItems} style={{ marginTop: '20px' }} />
       )}
     </Modal>
   );
